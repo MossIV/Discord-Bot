6fb9539e56cc9ef6ddc059f8184d181dc317e52c
MossIV <waben.wong@gmail.com>
2025-08-27 11:16:34 +1200

    created a function on_message
    Created a function to respond to messages

--- pythonBot.py
+++ pythonBot.py
@@ -9,8 +9,15 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 print(DISCORD_TOKEN)
 
 class MyClient(discord.Client):
-  async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
 
 intents = discord.Intents.default()
 intents.message_content = True
