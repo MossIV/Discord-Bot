3bf384642306b3119d1db5d0d2d276756a3ae0f3
MossIV <waben.wong@gmail.com>
2025-08-27 18:52:16 +1200

    Integrated dad joke API

--- pythonBot.py
+++ pythonBot.py
@@ -9,9 +9,22 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 def get_meme():
-        response = requests.get('https://meme-api.com/gimme')
-        json_data = json.loads(response.text)
-        return json_data['url']
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    print(json_data)
+    return json_data['url']
+
+def get_dad():
+    URL = "https://icanhazdadjoke.com/"
+    headers = {
+        "Accept": "application/json",  # get JSON instead of HTML
+        "User-Agent": "Discord Python Bot"  # identify your app
+    }
+
+    response = requests.get(URL, headers=headers)
+    json_data = response.json()
+    return json_data['joke']
+    
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -24,6 +37,9 @@ class MyClient(discord.Client):
         if message.content.startswith('$meme'):
             await message.channel.send(get_meme())
 
+        if message.content.startswith('$dad'):
+            await message.channel.send(get_dad())
+        
 
 intents = discord.Intents.default()
 intents.message_content = True
